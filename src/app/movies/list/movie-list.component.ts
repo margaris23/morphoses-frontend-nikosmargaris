501c3b534f2f642358eb1a39fc46ffd960508a5a
Nikolas Margaris <reverence23@gmail.com>
2025-06-05 08:56:34 +0300

    movie search fix

--- src/app/movies/list/movie-list.component.ts
+++ src/app/movies/list/movie-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, inject, afterNextRender, OnDestroy } from '@angular/core';
 import { MoviesService } from '../movies.service';
 import { AsyncPipe } from '@angular/common';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-movie-list',
@@ -8,7 +9,36 @@ import { AsyncPipe } from '@angular/common';
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.css'
 })
-export class MovieListComponent {
+export class MovieListComponent implements OnDestroy {
   private movieService = inject(MoviesService);
-  protected movies$ = this.movieService.movies();
+
+  private observer!: IntersectionObserver;
+  protected movies$ = this.movieService.movies().pipe(tap(res => {
+    console.log('GOT NEW RESULTS:', res?.length);
+  }));
+
+  constructor() {
+    const elementRef = inject(ElementRef);
+
+    afterNextRender(() => {
+      const root = elementRef.nativeElement;
+
+      this.observer = new IntersectionObserver(
+        ([entry]: IntersectionObserverEntry[]) => {
+          if (entry.isIntersecting) {
+            this.movieService.loadMore();
+          }
+        },
+        { root, rootMargin: "0%", threshold: 1.0 }
+      );
+
+      this.observer.observe(root.querySelector("#loading"));
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+  }
 }
--- src/app/movies/movies.service.ts
+++ src/app/movies/movies.service.ts
@@ -1,39 +1,78 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
-import { map, Observable, switchMap } from 'rxjs';
+import { inject, Injectable } from '@angular/core';
+import { map, Observable, switchMap, BehaviorSubject, combineLatest, scan, distinctUntilChanged } from 'rxjs';
 import { Movie, MovieItem, toMovieItem } from './movies.model';
 import { environment as env } from '../../environments/environment';
 import { APIListResult } from '../api.model';
-import { toObservable } from '@angular/core/rxjs-interop';
 
 const API = {
   NOW_PLAYING: '/api/movie/now_playing',
   SEARCH: '/api/search/movie',
 };
 
-@Injectable({
-  providedIn: 'root'
-})
+const FIRST_PAGE = 1;
+
+@Injectable({ providedIn: 'root' })
 export class MoviesService {
   private http = inject(HttpClient);
-
-  private _query = signal("");
-  private query$ = toObservable(this._query);
+  private query$ = new BehaviorSubject<string>("");
+  private page$ = new BehaviorSubject<number>(FIRST_PAGE);
 
   movies(): Observable<MovieItem[]> {
-    return this.query$.pipe(
-      switchMap(query => {
-        const auth = `api_key=${env.API_KEY}`;
-        const endp = query
-          ? `${API.SEARCH}?query=${query}&${auth}`
-          : `${API.NOW_PLAYING}?${auth}`;
-        return this.http.get<APIListResult<Movie>>(endp);
+    return combineLatest([this.query$, this.page$]).pipe(
+      // check if filtering changed
+      distinctUntilChanged((prev, current) => {
+        const [prevQuery, prevPage] = prev;
+        const [currQuery, currPage] = current;
+        return prevQuery === currQuery && prevPage === currPage;
       }),
-      map(res => res.results.map(toMovieItem))
+      // reset page if query changed
+      scan((acc, current) => {
+        const [prevQuery] = acc;
+        const [currQuery] = current;
+        return prevQuery === currQuery
+          ? current
+          : [currQuery, FIRST_PAGE] satisfies [string, number];
+      }),
+      // consume http API
+      switchMap(([query, page]) =>
+        this.fetchMovies(query, page).pipe(
+          map(reply => ({
+            results: reply.results.map(toMovieItem),
+            page: reply.page,
+            query
+          })))
+      ),
+      // accumulate results if page changed
+      scan((acc, res) => {
+        if (res.page > acc.page) {
+          return {
+            results: [...acc.results, ...res.results],
+            page: res.page,
+            query: res.query
+          };
+        }
+        return res;
+      }),
+      map(res => res.results)
     );
   }
 
   search(query: string): void {
-    this._query.set(query);
+    this.query$.next(query);
+  }
+
+  loadMore(): void {
+    this.page$.next(this.page$.value + 1);
+  }
+
+  private fetchMovies(query: string, page: number): Observable<APIListResult<Movie>> {
+    const params = new URLSearchParams();
+    params.set("api_key", env.API_KEY);
+    params.set("query", query);
+    params.set("page", page.toString());
+
+    const endp = `${query ? API.SEARCH : API.NOW_PLAYING}?${params}`;
+    return this.http.get<APIListResult<Movie>>(endp);
   }
 }
